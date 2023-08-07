fc78af6ee46ea972d50dd50d00510dd58f9c6d4a
Rox <67151410+RoxGamer@users.noreply.github.com>
2023-08-07 08:39:38 +0530

    Add files via upload

---
+++ faces_train.py
@@ -0,0 +1,35 @@
+import os
+import cv2 as cv
+import numpy as np
+
+people = ['Prabhjot','Sidak','Ishaan']
+DIR = r'E:\Project CV\AI_Project\photos\facep'
+haar_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+features =[]
+labels =[]
+
+
+def create_train(): 
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+        for img in os.listdir(path):
+            img_path =os.path.join(path,img)
+            img_array= cv.imread(img_path)
+            gray = cv.cvtColor(img_array,cv.COLOR_BGR2GRAY)
+            faces_rect = haar_cascade.detectMultiScale(gray,1.1,4)
+            for(x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+create_train()
+print('-----training done-----')
+features= np.array(features,dtype='object')
+labels= np.array(labels)
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+# train the recognizer on the features list and on the labels 
+face_recognizer.train(features,labels)
+face_recognizer.save('face_trained.yml')
+np.save('features.npy',features)
+np.save('labels.npy',labels)
